e17c96621e25d1e8da7a10a9e85ab0a1d961731b
Aakarsh Nair <aakarsh.nair@gmail.com>
2017-03-28 04:28:20 +0000

    Stackless tree height.

--- week-1/tree_height/tree-height.cc
+++ week-1/tree_height/tree-height.cc
@@ -1,3 +1,6 @@
+#include <utility> 
+#include <stack> 
+#include <cassert>
 #include <algorithm>
 #include <iostream>
 #include <vector>
@@ -5,49 +8,103 @@
 #include <sys/resource.h>
 #endif
 
+#ifdef DEBUG
+const bool debug = true;
+#else
+const bool debug = false;
+#endif
+
+using  std::pair;
+
 class Node;
 
 class Node {
 public:
-    int key;
-    Node *parent;
-    std::vector<Node *> children;
+  int key;
+  Node *parent;
+  std::vector<Node *> children;
 
-    Node() {
-      this->parent = NULL;
-    }
+  Node() {
+    this->parent = NULL;
+  }
 
-    void setParent(Node *theParent) {
-      parent = theParent;
-      parent->children.push_back(this);
-    }
+  void setParent(Node *theParent) {
+    parent = theParent;
+    parent->children.push_back(this);
+  }
+  bool is_leaf() { this->children.empty(); }
 };
 
 
 int main_with_large_stack_space() {
+  
   std::ios_base::sync_with_stdio(0);
   int n;
   std::cin >> n;
 
+
+  // vector of nodes - represent each leaf 
   std::vector<Node> nodes;
   nodes.resize(n);
+  int root;
+  // iterating through every node
   for (int child_index = 0; child_index < n; child_index++) {
+    // for current child node read parent index
     int parent_index;
     std::cin >> parent_index;
+    // set node<child_index>'s parent to be read value
+    // -1 will have no parent since it is the root
     if (parent_index >= 0)
       nodes[child_index].setParent(&nodes[parent_index]);
+    else
+      root = child_index;
+    
     nodes[child_index].key = child_index;
   }
-
-  // Replace this code with a faster implementation
   int maxHeight = 0;
-  for (int leaf_index = 0; leaf_index < n; leaf_index++) {
-    int height = 0;
-    for (Node *v = &nodes[leaf_index]; v != NULL; v = v->parent)
-      height++;
-    maxHeight = std::max(maxHeight, height);
-  }
+  
+  /**
+   * Instead we can start from root and do a depth first traversal and
+   * find compute height.
+   */
+  std::stack<pair<long,Node*>> stacked;
+  stacked.push(std::make_pair(0,&nodes[root]));
+
+  while( !stacked.empty() ) {
+    // need reference to top to modify processed child index 
+    pair<long,Node*> *cur  = &(stacked.top());
+    long child_pos = cur->first;
+    Node* node = cur->second;
     
+    if(debug) {
+      std::cerr<<"Stack top ["<<node<<"] num children "<<node->children.size()<<"child pos ["<<child_pos<<"]"<<std::endl;
+    }
+    
+    if( node->children.size() > 0 ) {
+      
+      if( child_pos < node->children.size() ) {
+        // better off with node child iterator? 
+        Node* child = node->children[child_pos];
+        
+        stacked.push(std::make_pair(0,child));
+        if(debug)
+          std::cerr<<"pushing "<<child->key <<std::endl;
+        
+        // increment child position,ugly! 
+        cur->first+=1;
+        
+        continue;
+      }
+      
+    } else if (node->is_leaf()) {
+      maxHeight = std::max((int)stacked.size() , maxHeight);
+      if(debug)
+        std::cerr<<"height : "<<maxHeight<<" stack "<<stacked.size()<<std::endl;
+    }
+    
+    stacked.pop();
+  }
+  
   std::cout << maxHeight << std::endl;
   return 0;
 }
@@ -62,17 +119,17 @@ int main (int argc, char **argv)
 
   result = getrlimit(RLIMIT_STACK, &rl);
   if (result == 0)
-  {
+    {
       if (rl.rlim_cur < kStackSize)
-      {
+        {
           rl.rlim_cur = kStackSize;
           result = setrlimit(RLIMIT_STACK, &rl);
           if (result != 0)
-          {
+            {
               std::cerr << "setrlimit returned result = " << result << std::endl;
-          }
-      }
-  }
+            }
+        }
+    }
 
 #endif
   return main_with_large_stack_space();
