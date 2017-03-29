2037d2be31d0e5b97241c062b0633e593dfc4003
Aakarsh Nair <aakarsh.nair@gmail.com>
2017-03-29 01:37:29 +0000

    job queue first pass.

--- week-2/job_queue/job_queue.cc
+++ week-2/job_queue/job_queue.cc
@@ -1,13 +1,19 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <queue>
+#include <utility>
 
+using std::pair;
+using std::priority_queue;
 using std::vector;
 using std::cin;
 using std::cout;
 
 class JobQueue {
+
  private:
+  
   int num_workers_;
   vector<int> jobs_;
 
@@ -28,30 +34,66 @@ class JobQueue {
       cin >> jobs_[i];
   }
 
+  class pair_compare {
+  public:
+     bool operator()(const pair<long,long> & w1 , const pair<long,long> & w2 ) {
+      return w1.second > w2.second;
+    }
+  };
+
   void AssignJobs() {
-    // TODO: replace this code with a faster algorithm.
+    
     assigned_workers_.resize(jobs_.size());
     start_times_.resize(jobs_.size());
+    
     vector<long long> next_free_time(num_workers_, 0);
-    for (int i = 0; i < jobs_.size(); ++i) {
-      int duration = jobs_[i];
-      int next_worker = 0;
-      for (int j = 0; j < num_workers_; ++j) {
-        if (next_free_time[j] < next_free_time[next_worker])
-          next_worker = j;
-      }
-      assigned_workers_[i] = next_worker;
-      start_times_[i] = next_free_time[next_worker];
-      next_free_time[next_worker] += duration;
+    
+    typedef std::priority_queue<pair<long,long>,vector<pair<long,long>>,pair_compare> heap_t;
+    heap_t worker_queue;
+    
+    int dawn_of_time = 0;
+    for(long i = 0; i < num_workers_; i++){
+      worker_queue.push(std::make_pair(i,dawn_of_time));
     }
+    
+    /**
+     * Using a priority queue:
+     *
+     * 1. Find workers with minimum next_free_time
+     * 2. Assign the new worker to this job. 
+     * 3. Add job duration to worker finish time add back to the priority queue.
+     */
+    for(long i = 0; i < jobs_.size(); i++) {
+      int duration = jobs_[i];
+      
+      pair<long,long> worker = worker_queue.top(); worker_queue.pop();
+      
+      long worker_id = worker.first;
+      long worker_finish_time = worker.second;
+     
+      assigned_workers_[i] = worker_id;
+      
+      // start time for this job will be when the worker that
+      // was assigned to it finishes.
+      start_times_[i] = worker_finish_time;
+
+      worker_finish_time += duration;
+      
+      // enqueue the worker back to the queue.
+      pair<long,long> updated_worker = std::make_pair(worker.first,worker_finish_time);
+      worker_queue.push(updated_worker);
+    }
+    
   }
 
  public:
+  
   void Solve() {
     ReadData();
     AssignJobs();
     WriteResponse();
   }
+  
 };
 
 int main() {
