045ee96aa40853f9197c2243797eef4ef439a18a
Aakarsh Nair <aakarsh.nair@gmail.com>
2017-03-30 03:53:59 +0000

    overflow bug?

--- week-3/hash_chains/hash_chains.cc
+++ week-3/hash_chains/hash_chains.cc
@@ -1,13 +1,25 @@
+#include <cassert>
 #include <iostream>
 #include <string>
 #include <list>
 #include <vector>
+#include <map>
+#include <utility>
 #include <algorithm>
+#include <cstdlib>
 
 using std::list;
 using std::string;
 using std::vector;
 using std::cin;
+using std::cerr;
+using std::cout;
+
+#ifdef DEBUG
+const bool debug = true;
+#else
+const bool debug = false;
+#endif
 
 struct Query {
   string type;
@@ -32,44 +44,58 @@ class QueryProcessor {
    *
    * s[i] is based off the i'th character of string
    */
-  size_t hash_func(const string& s) const {
-    static const size_t multiplier = 263;
-    static const size_t prime = 1000000007;
+  long long hash_func(const string& s) const {
+    static const long long multiplier = 263;
+    static const long long prime = 1000000007;
     
     unsigned long long hash = 0;
     
-    for (int i = static_cast<int> (s.size()) - 1; i >= 0; --i)
+    for (long long i = static_cast<long long> (s.size()) - 1; i >= 0; --i)
       hash = (hash * multiplier + s[i]) % prime;
     
     return hash % bucket_count;
   }
 
+  inline long long idx(const string & str) {
+    return static_cast<long long>(hash_func(str));
+  }
+  
+private:
+  
+  inline long long next_bucket_count(long long in){
+    long long i = 1;
+    for( i = 1; i < in; i *= 2);
+    return i;
+  }
+
+  void resize(long long sz) {
+    vector<list<string>>& hash_table = *table; 
+    
+    if(bucket_count < sz)
+      bucket_count = next_bucket_count(sz); 
+    
+    vector<list<string>>* new_table = new vector<list<string>>(bucket_count,list<string>());
+    
+    for(auto const & chain : hash_table) 
+      for(auto const & e  : chain) 
+        (*new_table)[idx(e)].push_front(e); 
+        
+    delete table;
+    this->table = new_table;
+  }
+
   
 public:
   
-  explicit QueryProcessor(int bucket_count):
-    bucket_count(bucket_count) {
+  explicit QueryProcessor(int bucket_count): bucket_count(bucket_count) {
+    bucket_count = next_bucket_count(bucket_count);
     table = new vector<list<string>>(bucket_count,list<string>());
   }
   
   ~QueryProcessor(){
     delete table;
   }
-  
-  Query readQuery() const {
-    Query query;
-    cin >> query.type;
-    if (query.type != "check")
-      cin >> query.s;
-    else
-      cin >> query.ind;
-    return query;
-  }
 
-  inline void writeSearchResult(bool was_found) const {
-    std::cout << (was_found ? "yes" : "no")<<std::endl;;
-  }
-  
   /**
    * Add str to the hash table. if table is insufficient size.
    * resize it.
@@ -83,58 +109,55 @@ public:
     
     hash_table[id].push_front(str);
   }
-
-  void resize(long long sz) {
-    vector<list<string>>& hash_table = *table; 
-    
-    while(bucket_count < sz) bucket_count *= 2;
-    
-    vector<list<string>>* new_table = new vector<list<string>>(bucket_count,list<string>());
-    
-    for(auto const & chain : hash_table) {
-      for(auto const & e  : chain) {
-        (*new_table)[idx(e)].push_front(e); 
-      }
-    }
-    
-    delete table;
-    this->table = new_table;
-  }
-
-  inline long long idx(const string & str) {
-    return static_cast<long long>(hash_func(str));
-  }
   
-  inline bool find(const string & str) {
+  /**
+   * true - there exists a occurance of str in hashed bucket.
+   */
+  inline bool find_string(const string & str) {
     vector<list<string>>& hash_table = *table; 
     list<string>& chain = hash_table[idx(str)];
+    
     for(auto const & elem : chain)
       if(elem == str) return true;
+    
     return false;
   }
   
+  /**
+   * Delete all occurances of str from hashed bucket.
+   */  
+  void delete_string(const string & str) {
+    vector<list<string>>& hash_table = *table; 
+    list<string>& chain = hash_table[idx(str)];
+    for(auto it = chain.begin(); it!=chain.end();){
+      (str == *it) ? chain.erase(it++) : it++;
+    }
+  }
+  
+  Query readQuery() const {
+    Query query;
+    cin >> query.type;
+    (query.type != "check") ? cin >> query.s : cin >> query.ind;
+    return query;
+  }
+
+  inline void writeSearchResult(bool was_found) const {
+    std::cout<< (was_found ? "yes" : "no")<<std::endl;;
+  }
+   
   void processQuery(const Query& query) {
     vector<list<string>>& hash_table = *table; 
-    
     if (query.type == "check") { // space seperated chain mapped to same key 
-      list<string>& chain = hash_table[query.ind];
-      for(auto const & elem : chain)
+      for(auto const & elem : hash_table[query.ind])
         std::cout<<elem<<" ";
       std::cout <<std::endl;
-    } else {
-      
-      if (query.type == "find") {
-        writeSearchResult(find(query.s));
-      } else if (query.type == "add") {
-        add_string(query.s);
-      } else if (query.type == "del") {
-        list<string>& chain = hash_table[idx(query.s)];
-        for(auto it = chain.begin(); it!=chain.end();){
-          (query.s == *it) ? chain.erase(it++) : it++;
-        }
- 
-      }
-    }
+    } else if (query.type == "find") {
+      writeSearchResult(find_string(query.s));
+    } else if (query.type == "add") {
+      add_string(query.s);
+    } else if (query.type == "del") {
+      delete_string(query.s);
+    } // ignore unknown commands
   }
 
   void processQueries() {
@@ -145,6 +168,37 @@ public:
   }
   
 };
+
+string random_string(int len) {
+  string retval(len,' ');
+  static const char char_set[] =
+    "0123456789"
+    "abcdefghijklmnopqrstuvwxyz"
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  for(int i = 0 ;i< len; i++)
+    retval[i] = char_set[rand() % (sizeof(char_set)-1)];
+  return retval;
+}
+
+
+void test() {
+  QueryProcessor proc(10);
+  std::map<string,bool> map;
+  
+  for(int i = 0 ; i < 50; i++) {
+    string r =random_string(10); 
+    map[r] = true; 
+    proc.add_string(r);
+  }
+  
+  for(const auto& entry : map) {
+    assert(proc.find_string(entry.first));
+    proc.delete_string(entry.first);
+    assert(!proc.find_string(entry.first));
+  }
+  
+}
 
 int main() {
   std::ios_base::sync_with_stdio(false);
