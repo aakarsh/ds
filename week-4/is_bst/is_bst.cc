ad80bb5c36761d20ac44d56c58f71e323eb7865b
Aakarsh Nair <aakarsh.nair@gmail.com>
2017-03-31 02:51:34 +0000

    basic bst stuff.

--- week-4/is_bst/is_bst.cc
+++ week-4/is_bst/is_bst.cc
@@ -11,13 +11,27 @@ struct Node {
   int key;
   int left;
   int right;
-
   Node() : key(0), left(-1), right(-1) {}
   Node(int key_, int left_, int right_) : key(key_), left(left_), right(right_) {}
 };
 
-bool IsBinarySearchTree(const vector<Node>& tree) {
-  // Implement correct algorithm here
+bool is_bst(const vector<Node>& tree,int cur) {
+  if(tree.empty()) return true;
+  
+  Node cur_nd = tree[cur];
+  
+  if(cur_nd.left >=0  && !(tree[cur_nd.left].key < tree[cur].key))
+    return false;
+  
+  if(cur_nd.right >=0  && !(tree[cur_nd.right].key >= tree[cur].key))
+    return false;
+  
+  if(cur_nd.left >= 0 && !is_bst(tree,cur_nd.left))
+     return false;
+     
+  if(cur_nd.right >= 0 && !is_bst(tree,cur_nd.right))
+     return false;
+  
   return true;
 }
 
@@ -30,10 +44,7 @@ int main() {
     cin >> key >> left >> right;
     tree.push_back(Node(key, left, right));
   }
-  if (IsBinarySearchTree(tree) {
-    cout << "CORRECT" << endl;
-  } else {
-    cout << "INCORRECT" << endl;
-  }
+  const char* bst_msg[] = {"INCORRECT","CORRECT" };
+  cout<< bst_msg[is_bst(tree,0)] <<endl; 
   return 0;
 }
