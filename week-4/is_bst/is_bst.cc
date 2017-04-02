92565feec3a35b8b8f1a07cc6dad39a465b51301
Aakarsh Nair <aakarsh.nair@gmail.com>
2017-04-02 13:18:24 +0000

    non recursive traversal

--- week-4/is_bst/is_bst.cc
+++ week-4/is_bst/is_bst.cc
@@ -1,8 +1,10 @@
+#include <stack>
 #include <algorithm>
 #include <utility>
 #include <iostream>
 #include <vector>
 
+using std::stack;
 using std::pair;
 using std::make_pair;
 using std::cin;
@@ -17,17 +19,100 @@ const bool debug = false;
 #endif
 
 struct Node {
-  int key;
+  long long key;
   int left;
   int right;
+  bool visited = 0;
+  int pos  = -1;
   Node() : key(0), left(-1), right(-1) {}
-  Node(int key_, int left_, int right_) : key(key_), left(left_), right(right_) {}
+  Node(long long key_, int left_, int right_) : key(key_), left(left_), right(right_) {}
 };
 
-typedef pair<int,int> range;
+typedef pair<long long,long long> range;
+
+void in_order(const vector<Node>& tree, int cur){
+  if(tree.empty())
+    return;
+  Node n = tree[cur];
+  int left = n.left;
+  int right = n.right;
+  
+  if(left >= 0)
+    in_order(tree,left);
+  
+  cout<<" "<<n.key<<" ";
+  
+  if(right>=0)
+    in_order(tree,right);
+  
+}
+
+void in_order_stackless( vector<Node>& tree,int idx){
+  
+  if(tree.empty())
+    return;
+  
+  stack<Node*> s;
+  Node root = tree[idx];
+  
+  s.push(&root);
+  while(!s.empty()) {
+    
+    Node * cur_p = s.top(); s.pop();
+    Node & cur = *cur_p;
+    if( cur.left == -1 || tree[cur.left].visited ) { // ready process current node
+      
+      std::cout<<" "<<cur.key<<" ";
+      cur.visited = true;
+      
+      if(cur.right >= 0) { // add the right child for processing. 
+        s.push(&tree[cur.right]);
+      }
+      
+    } else { // unvisited left child
+      s.push(&cur); // delay processing current
+      s.push(&tree[cur.left]);
+    }
+  }
+}
+
+void in_order_nonrecurse(vector<Node> & tree, int idx) {
+  
+  if(tree.empty()) return;
+  
+  for(auto &n : tree) n.visited = false;
+  
+  stack<Node> s;
+  Node cur =tree[idx];
+  s.push(cur);
+  
+  while(!s.empty()) {
+    
+    cur = s.top(); s.pop();
+    
+    while(cur.left != -1 && !(tree[cur.left].visited)  ) { // go-left son 
+      s.push(tree[cur.pos]);
+      cur = tree[cur.left];
+    }
+    
+    // Exhausted left sub tree, visit current
+    std::cerr<<" "<< cur.key<<" ";
+    tree[cur.pos].visited = true;
+    
+    if(cur.right != -1 ) {// has a right child 
+      cur = tree[cur.right];
+      s.push(tree[cur.pos]);
+    }
+  }
+  
+  for(auto &n : tree) n.visited = false;
+  
+  std::cerr<<endl;
+}
 
 /**
  * Return rightmost child as well as if it is tree.
+ j
  */
 pair<bool,range>is_bst (const vector<Node>& tree,int cur) {
   
@@ -41,16 +126,21 @@ pair<bool,range>is_bst (const vector<Node>& tree,int cur) {
   range right_range = make_pair(cur_nd.key,cur_nd.key);
 
   if(cur_nd.left >=0  && !(tree[cur_nd.left].key < cur_nd.key)) {
+    
     if(debug)
       std::cerr<<"for node "<<cur_nd.key<<" left child more "<<tree[cur_nd.left].key<<std::endl;
+    
     return make_pair(false,empty_range);
   }
   
   if(cur_nd.right >=0  && !(tree[cur_nd.right].key >= cur_nd.key)) {
+    
     if(debug)
       std::cerr<<"for node "<<cur_nd.key<<" right child less "<<tree[cur_nd.right].key<<std::endl;
+    
     return make_pair(false,empty_range);
-  } 
+  }
+  
   if(cur_nd.left >= 0) {
     pair<bool,range> p = is_bst(tree,cur_nd.left);
     bool left_bst = p.first;
@@ -84,8 +174,18 @@ int main() {
   for (int i = 0; i < nodes; ++i) {
     int key, left, right;
     cin >> key >> left >> right;
-    tree.push_back(Node(key, left, right));
+    Node n = Node(key, left, right);
+    n.pos = i;
+    n.visited = false;
+    tree.push_back(n);
   }
+  if(debug){ 
+    in_order(tree,0);
+    std::cout<<endl;
+    in_order_stackless(tree,0);
+    std::cout<<std::endl;
+    in_order_nonrecurse(tree,0);
+  }  
   const char* bst_msg[] = {"INCORRECT","CORRECT" };
   cout<< bst_msg[is_bst(tree,0).first] <<endl; 
   return 0;
